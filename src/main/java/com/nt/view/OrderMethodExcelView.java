9337e9b78d00756edfa5be2ca71d875c9fdeb241
imran7250 <imranahmad9942880@gmail.com>
2026-07-16 10:04:59 +0530

    Update OrderMethodExcelView.java

--- src/main/java/com/nt/view/OrderMethodExcelView.java
+++ src/main/java/com/nt/view/OrderMethodExcelView.java
@@ -5,7 +5,7 @@ import java.util.Map;
 
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.apache.poi.ss.usermodel.Workbook;
 import org.springframework.web.servlet.view.document.AbstractXlsxView;
 
 import com.nt.model.OrderMethod;
@@ -18,27 +18,26 @@ public class OrderMethodExcelView extends AbstractXlsxView {
     @Override
     protected void buildExcelDocument(
             Map<String, Object> model,
-            XSSFWorkbook workbook,
+            Workbook workbook,
             HttpServletRequest request,
             HttpServletResponse response) throws Exception {
 
         response.addHeader("Content-Disposition",
-                "attachment;filename=OrderMethods.xlsx");
+                "attachment; filename=OrderMethods.xlsx");
 
         Sheet sheet = workbook.createSheet("ORDER METHODS");
 
         Row header = sheet.createRow(0);
 
         header.createCell(0).setCellValue("ID");
-        header.createCell(1).setCellValue("MODE");
-        header.createCell(2).setCellValue("CODE");
-        header.createCell(3).setCellValue("TYPE");
-        header.createCell(4).setCellValue("ACCEPT");
+        header.createCell(1).setCellValue("ORDER MODE");
+        header.createCell(2).setCellValue("ORDER CODE");
+        header.createCell(3).setCellValue("ORDER TYPE");
+        header.createCell(4).setCellValue("ORDER ACCEPT");
         header.createCell(5).setCellValue("DESCRIPTION");
 
         @SuppressWarnings("unchecked")
-        List<OrderMethod> list =
-                (List<OrderMethod>) model.get("list");
+        List<OrderMethod> list = (List<OrderMethod>) model.get("list");
 
         int rowNum = 1;
 
@@ -51,10 +50,17 @@ public class OrderMethodExcelView extends AbstractXlsxView {
             row.createCell(2).setCellValue(om.getOrderCode());
             row.createCell(3).setCellValue(om.getOrderType());
 
-            row.createCell(4).setCellValue(
-                    String.join(",", om.getOrderAcpt()));
+            if (om.getOrderAcpt() != null) {
+                row.createCell(4).setCellValue(String.join(", ", om.getOrderAcpt()));
+            } else {
+                row.createCell(4).setCellValue("");
+            }
 
             row.createCell(5).setCellValue(om.getDescription());
+        }
+
+        for (int i = 0; i < 6; i++) {
+            sheet.autoSizeColumn(i);
         }
     }
 }
