6b129bdece32481dab4b99d4cb746b236282f8a1
imran7250 <imranahmad9942880@gmail.com>
2026-07-16 15:03:43 +0530

    Update PartController.java

--- src/main/java/com/nt/controller/PartController.java
+++ src/main/java/com/nt/controller/PartController.java
@@ -2,7 +2,7 @@
 
 // import java.util.List;
 
-// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.beans.factory.annotation.Autowired; 
 // import org.springframework.data.domain.Page;
 // import org.springframework.data.domain.Pageable;
 // import org.springframework.data.web.PageableDefault;
