a2030983190d6db5207badc59535477210c509d4
imran7250 <imranahmad9942880@gmail.com>
2026-08-09 18:03:56 +0530

    Update Document.java

--- src/main/java/com/nt/model/Document.java
+++ src/main/java/com/nt/model/Document.java
@@ -3,6 +3,7 @@ package com.nt.model;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;   // <-- ADD THIS
 import jakarta.persistence.Id;
 import jakarta.persistence.Lob;
 import jakarta.persistence.Table;
@@ -13,14 +14,15 @@ import lombok.Data;
 @Table(name="docs_tab")
 public class Document {
 
-	@Id
+    @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name="doc_id")
-	private Integer docId;
-	@Column(name="doc_name")
-	private String docName;
-	@Lob
-	@Column(name="doc_data", columnDefinition = "LONGBLOB")
-	private byte[] docData;
-	
+    private Integer docId;
+
+    @Column(name="doc_name")
+    private String docName;
+
+    @Lob
+    @Column(name="doc_data", columnDefinition = "LONGBLOB")
+    private byte[] docData;
 }
